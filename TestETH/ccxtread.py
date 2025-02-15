5aaed1aee4da776cc70cbd4e767cee1f539fb7a5
Andy Han <hanxu6010@gmail.com>
2025-02-15 11:44:39 -0800

    300 data

--- TestETH/ccxtread.py
+++ TestETH/ccxtread.py
@@ -8,7 +8,7 @@ exchange = exchange = ccxt.coinbase()
 # Define symbol and timeframe
 symbol = "ETH/USDT"  # ETH price in USDT
 timeframe = "1h"  # 1-hour interval
-since = exchange.parse8601((datetime.utcnow() - timedelta(days=7)).isoformat())  # Past week
+since = exchange.parse8601((datetime.utcnow() - timedelta(days=21)).isoformat())  # Past week
 
 # Fetch historical OHLCV (Open, High, Low, Close, Volume) data
 ohlcv = exchange.fetch_ohlcv(symbol, timeframe, since=since)
@@ -24,3 +24,4 @@ df = df[["timestamp", "open", "close"]]
 
 # Print the DataFrame
 print(df)  # Show first 10 rows
+df.to_csv("ETH.csv")
